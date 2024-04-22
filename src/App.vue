187cc1cbca66d36674cb7ce095c6ff88759dbddd
Kunniii <truongnp@codechovui.dev>
2024-04-22 22:35:52 +0700

    simple webite using VUE

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup></script>
+<template>
+  <div class="flex flex-col text-center items-center pt-24 h-screen">
+    <h1 class="text-5xl font-main font-light">spiderdev.vn</h1>
+    <img
+      src="/logo.jpg"
+      class="w-64 rounded-[3rem] my-10 shadow-lg hover:shadow-xl hover:scale-105 duration-200"
+    />
+    <h2 class="font-main font-light">Website spiderdev đang được chúng mình hoàn thiện</h2>
+    <h2 class="font-main font-light">Mọi người quay lại sau nhé!</h2>
+
+    <div class="bg-gradient-to-tl from-blue-800 to-teal-300 rounded-full my-10">
+      <img
+        src="./assets/rolling.svg"
+        class="animate-spin"
+      />
+    </div>
+  </div>
+</template>
